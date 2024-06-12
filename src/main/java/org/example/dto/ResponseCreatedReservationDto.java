1d6bd2d1695c9ece8920fc38bf8334b4df1232f6
st61057 <st61057@upce.cz>
2024-06-12 10:26:48 +0200

    remove unused dto

--- src/main/java/org/example/dto/ResponseCreatedReservationDto.java
+++
@@ -1,9 +0,0 @@
-package org.example.dto;
-
-import java.util.Date;
-
-public class ResponseCreatedReservationDto {
-    private String email;
-    private TimeSlotDto timeSlotDto;
-    private Date date;
-}
